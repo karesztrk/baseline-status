a3f679801a7f4035e503f8bf6b1d580e8508ca14
Károly Török <torok.karoly.krisztian@gmail.com>
2024-11-01 14:28:07 +0100

    Typescript support added

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
-import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import minifyHTML from 'rollup-plugin-html-literals';
-import summary from 'rollup-plugin-summary';
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import minifyHTML from "rollup-plugin-html-literals";
+import summary from "rollup-plugin-summary";
+import { defineConfig } from "rollup";
 
-export default {
-  input: 'baseline-status.js',
-  output: { file: 'baseline-status.min.js' },
+export default defineConfig({
+  input: "./src/baseline-status.js",
+  output: { file: "./dist/baseline-status.min.js" },
   plugins: [
     resolve(),
     minifyHTML(),
     terser({
       compress: {
-        drop_console: true
-      }
+        drop_console: true,
+      },
     }),
     summary(),
   ],
-};
+});
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import BaselineIcon from "./baseline-icon.js";
+import BaselineStatus from "./baseline-status.js";
+
+export default { BaselineIcon, BaselineStatus };
